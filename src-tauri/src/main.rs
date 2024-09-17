866653b1cab078f273f6de8ebc2ce723ced70eb1
David Bos <david.bos@outlook.com>
2024-09-17 19:27:29 +0200

    INIT

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,65 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use qrcode::QrCode;
+use qrcode::render::svg;
+use base64::encode;
+use tauri::{Config, State};
+use directories::UserDirs;
+use std::{fs::File, io::Write};
+
+#[tauri::command]
+fn get_downloads_path() -> Option<String> {
+    UserDirs::new()
+        .and_then(|user_dirs| user_dirs.download_dir().map(|path| path.to_path_buf()))
+        .map(|path_buf| path_buf.to_string_lossy().into_owned())
+}
+
+#[tauri::command]
+fn generate_qr_code(data: String, _config: State<'_, Config>) -> Result<String, String> {
+    // Genereer de QR-code
+    let code = QrCode::new(data).map_err(|e| e.to_string())?;
+
+    // De gerenderde QR-code als SVG
+    let svg_code: String = code.render::<svg::Color>().min_dimensions(200, 200).build();
+
+    // Encodeer de SVG-code naar een base64-string
+    let base64_svg = encode(svg_code);
+
+    // Maak een data URL
+    let data_url = format!("data:image/svg+xml;base64,{}", base64_svg);
+    Ok(data_url)
+}
+
+#[tauri::command]
+fn generate_qr_code_export(data: String) -> Result<(), String> {
+    // Genereer de QR-code
+    let code = QrCode::new(data.clone()).map_err(|e| e.to_string())?;
+
+    // De gerenderde QR-code als SVG
+    let svg_code: String = code.render::<svg::Color>().min_dimensions(200, 200).build();
+
+    // Verkrijg het pad naar de download directory
+    let downloads_path = get_downloads_path().ok_or("Unable to get downloads path")?;
+    
+    // Maak de volledige bestandsnaam en pad aan
+    let file_name = format!("qr_{}.svg", data);
+    let file_path = format!("{}/{}", downloads_path, file_name);
+    
+    // Sla het SVG-bestand op
+    let mut file = File::create(file_path).map_err(|e| e.to_string())?;
+    file.write_all(svg_code.as_bytes()).map_err(|e| e.to_string())?;
+
+    Ok(())
+}
+
+
+fn main() {
+    tauri::Builder::default()
+        .manage(Config::default())
+        .invoke_handler(tauri::generate_handler![generate_qr_code , get_downloads_path, generate_qr_code_export])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/Scripts/index.js
@@ -0,0 +1,53 @@
+const { invoke } = window.__TAURI__.tauri;
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    const popup = document.getElementById("popup");
+    const ptext = document.getElementById("ptext");
+    popup.style.visibility = "hidden";
+
+    
+    document.getElementById('Generate').addEventListener('click', async () => {
+        const data = document.getElementById('input-link').value;
+        if (!data) return;
+
+        try {
+            // Roep de Rust functie aan om de QR-code te genereren
+            const imagePath = await invoke('generate_qr_code', { data });
+
+            // Log de pad naar het bestand
+            console.log('Image Path:', imagePath);
+
+            // Toon de QR-code in de <img> tag
+            document.getElementById('Generated').src = imagePath;
+        } catch (error) {
+            console.error('Error generating QR code:', error);
+        }
+    });
+
+    document.getElementById('Export').addEventListener('click', async () => {
+        const downloadpath = await invoke("get_downloads_path");
+        console.log(downloadpath);
+
+        const data = document.getElementById('input-link').value;
+        if (!data) return;
+
+        try {
+            // Roep de Rust functie aan om de QR-code te genereren
+            const imagePath = await invoke('generate_qr_code_export', { data });
+
+            // Log de pad naar het bestand
+            console.log('Image Path:', imagePath);
+            console.log('Exported')
+
+            popup.style.visibility = "visible";
+            ptext.innerText = "Exported: " + downloadpath + "/qr_" + data + ".svg";
+
+            setTimeout(() => {
+                popup.style.visibility = "hidden";
+            }, 5000);
+        } catch (error) {
+            console.error('Error generating QR code:', error);
+        }
+    });
+});
